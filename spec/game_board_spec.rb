7b1f749a99a3246ccb39efc68d738ca76300632d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-22 23:18:34 -0700

    Expect a message to be output to the console

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -6,5 +6,8 @@ describe GameBoard do
   subject(:new_game) { GameBoard.new }
 
   describe '#start_game' do
+    it 'outputs message' do
+      expect(new_game.start_game).to receive(:puts).once
+    end
   end
 end
