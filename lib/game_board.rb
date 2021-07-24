64b123b058009847e559521d1c1d8780bea1c567
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-24 00:50:01 -0700

    Return input if valid in #validate_input

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -11,4 +11,8 @@ class GameBoard
   def create_player(name, symbol)
     Player.new(name, symbol)
   end
+
+  def validate_input(valid_input, valid_entries)
+    return valid_input if valid_entries.include?(valid_input)
+  end
 end
