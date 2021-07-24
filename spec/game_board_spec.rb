22a0ae5f7b5824b690009e4651cd138bc22f5aae
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-23 21:39:02 -0700

    Fix; Incorrect syntax for message expectation

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -7,7 +7,8 @@ describe GameBoard do
 
   describe '#start_game' do
     it 'outputs message' do
-      expect(new_game.start_game).to receive(:puts).once
+      expect(new_game).to receive(:puts).once
+      new_game.start_game
     end
   end
 end
