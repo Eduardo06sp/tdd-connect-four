146bdbf671ebbd821301d203eb9faeaa5f6a92d2
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-26 18:42:29 -0700

    Create a hash with keys A-G with numbers 1-6

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -5,5 +5,17 @@ class GameBoard
   end
 
   def create_board
+    board = {}
+    x = ('A'..'G').to_a
+
+    x.each do |letter|
+      i = 1
+      until i == 7
+        board["#{letter}#{i}"] = ' '
+        i += 1
+      end
+    end
+
+    board
   end
 end
