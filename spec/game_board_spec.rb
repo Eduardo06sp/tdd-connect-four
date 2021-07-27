dcfd8f8eea15c9fea43e3f071e1fab8f2147baa2
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-26 18:29:20 -0700

    Expect #create_board to return hash with 42 keys

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -4,5 +4,9 @@ require_relative '../lib/game_board'
 
 describe GameBoard do
   describe '#create_board' do
+    it 'returns a hash with 42 keys' do
+      board_keys = new_board.create_board.keys
+      expect(board_keys.count).to eq(42)
+    end
   end
 end
