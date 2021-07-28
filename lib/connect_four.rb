20cf56bd48530400bb19090e112ad581dea29c95
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-28 00:09:56 -0700

    Add getter method for board instance variable

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,7 +4,7 @@ require_relative 'game_board'
 
 class ConnectFour
   attr_accessor :turn
-  attr_reader :player_one, :player_two
+  attr_reader :board, :player_one, :player_two
 
   def initialize(player_one, player_two)
     @player_one = player_one
