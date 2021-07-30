e6d922d9c0ac31dfe7a6e7358eb8e57d855951d3
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-29 22:30:19 -0700

    Use method stub to test #end_game

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -213,6 +213,7 @@ describe ConnectFour do
       winner = new_game.instance_variable_get(:@winner)
       win_message = "Congratulations, #{winner}, you win!"
 
+      allow(new_game).to receive(:winner?).and_return(true)
       expect(new_game).to receive(:puts).with(win_message)
       new_game.end_game
     end
