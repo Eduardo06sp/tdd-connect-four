19bd367ed7753e37ed0225336951277325800e91
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-30 21:40:45 -0700

    Refactor; Use lowercase moves in #update_possible_moves tests

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -182,28 +182,28 @@ describe ConnectFour do
   describe '#update_possible_moves' do
     it 'updates A1 to A2 when gamepiece fills A1' do
       possible_moves = new_game.instance_variable_get(:@possible_moves)
-      last_move = 'A1'
+      last_move = 'a1'
 
       new_game.update_possible_moves(last_move)
-      expect(possible_moves).to eq(%w[A2 B1 C1 D1 E1 F1 G1])
+      expect(possible_moves).to eq(%w[a2 b1 c1 d1 e1 f1 g1])
     end
 
     it 'updates G3 to G4 when last move is G3' do
-      new_game.instance_variable_set(:@possible_moves, %w[A1 B1 C1 D1 E1 F1 G3])
+      new_game.instance_variable_set(:@possible_moves, %w[a1 b1 c1 d1 e1 f1 g3])
       possible_moves = new_game.instance_variable_get(:@possible_moves)
-      last_move = 'G3'
+      last_move = 'g3'
 
       new_game.update_possible_moves(last_move)
-      expect(possible_moves).to eq(%w[A1 B1 C1 D1 E1 F1 G4])
+      expect(possible_moves).to eq(%w[a1 b1 c1 d1 e1 f1 g4])
     end
 
     it 'removes element if last move is G7' do
-      new_game.instance_variable_set(:@possible_moves, %w[A1 B1 C1 D1 E1 F1 G7])
+      new_game.instance_variable_set(:@possible_moves, %w[a1 b1 c1 d1 e1 f1 g7])
       possible_moves = new_game.instance_variable_get(:@possible_moves)
-      last_move = 'G7'
+      last_move = 'g7'
 
       new_game.update_possible_moves(last_move)
-      expect(possible_moves).to eq(%w[A1 B1 C1 D1 E1 F1])
+      expect(possible_moves).to eq(%w[a1 b1 c1 d1 e1 f1])
     end
   end
 
