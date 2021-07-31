12ba93f02faa2a74728f8df612a7b4369a0575c9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-31 16:05:23 -0700

    Refactor; Expect #create_board to create hash with 7 keys

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -6,9 +6,9 @@ describe GameBoard do
   subject(:new_board) { GameBoard.new }
 
   describe '#create_board' do
-    it 'returns a hash with 42 keys' do
+    it 'returns a hash with 7 keys' do
       board_keys = new_board.create_board.keys
-      expect(board_keys.count).to eq(42)
+      expect(board_keys.count).to eq(7)
     end
   end
 
