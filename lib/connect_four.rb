b4d20b6d1b0e8a3711e98f300a212abb599bf5ab
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-31 18:34:49 -0700

    Refactor; Allow player to input columns to make play

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -10,7 +10,7 @@ class ConnectFour
   attr_reader :game_board, :player_one, :player_two
 
   def initialize(player_one, player_two)
-    @possible_moves = %w[a1 b1 c1 d1 e1 f1 g1]
+    @possible_moves = [1, 2, 3, 4, 5, 6]
     @player_one = player_one
     @player_two = player_two
     @game_board = GameBoard.new
