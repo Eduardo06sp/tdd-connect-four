04d28e0655d60d9257c2baf6513271e2e7689ca3
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-01 16:28:22 -0700

    Refactor; Delete unused method tests

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -15,13 +15,6 @@ describe ConnectFour do
     end
   end
 
-  describe '#start_game' do
-    it 'outputs message' do
-      expect(new_game).to receive(:puts).once
-      new_game.start_game
-    end
-  end
-
   describe '#create_player' do
     it 'creates a new Player instance with given arguments' do
       expect(Player).to receive(:new).with('Sam', 'white_token')
